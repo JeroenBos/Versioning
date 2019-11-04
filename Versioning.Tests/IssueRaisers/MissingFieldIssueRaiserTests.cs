92ab11e3abf96d848a7e866b33d4421f93efb2c8
JeroenBos <jer123bos@gmail.com>
2019-11-04 16:32:18 +0100

    Test MissingFieldIssueRaiser

---
+++ Versioning.Tests/IssueRaisers/MissingFieldIssueRaiserTests.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using Versioning.IssueRaisers;
+using Versioning.Issues;
+
+namespace Versioning.Tests
+{
+	public class MissingFieldIssueRaiserTests
+	{
+		private CompatiblityIssueCollector raiser = new MissingFieldIssueRaiser().ToSingleton();
+
+		[Test]
+		public void MissingFieldIsReported()
+		{
+			// arrange
+			Assembly a = AssemblyGenerator.Load("public class A { public int i; }").Assemblies.First();
+			Assembly b = AssemblyGenerator.Load("public class A { }").Assemblies.First();
+
+			// act
+			var issues = raiser.GetCompatibilityIssuesBetween(a, b).ToList();
+
+			Assert.AreEqual(1, issues.Count);
+		}
+
+		[Test]
+		public void NewFieldInNewAssemblyIsNotReported()
+		{
+			/// the difference with <see cref="MissingFieldIsReported"/> is the assembly contents are reversed
+
+			// arrange
+			Assembly a = AssemblyGenerator.Load("").Assemblies.First();
+			Assembly b = AssemblyGenerator.Load("public class A { public int i; }").Assemblies.First();
+
+			// act
+			var issues = raiser.GetCompatibilityIssuesBetween(a, b).ToList();
+
+			Assert.AreEqual(0, issues.Count);
+		}
+
+		[Test]
+		public void FieldMadePrivateIsReported()
+		{
+			// arrange
+			Assembly a = AssemblyGenerator.Load("public class A { public int i; }").Assemblies.First();
+			Assembly b = AssemblyGenerator.Load("public class A { int i; }").Assemblies.First();
+
+			// act
+			var issues = raiser.GetCompatibilityIssuesBetween(a, b).ToList();
+
+			Assert.AreEqual(1, issues.Count);
+		}
+
+		[Test]
+		public void FieldMadeProtectedIsReported()
+		{
+			// arrange
+			Assembly a = AssemblyGenerator.Load("public class A { public int i; }").Assemblies.First();
+			Assembly b = AssemblyGenerator.Load("public class A { protected int i; }").Assemblies.First();
+
+			// act
+			var issues = raiser.GetCompatibilityIssuesBetween(a, b).ToList();
+
+			Assert.AreEqual(1, issues.Count);
+		}
+
+		[Test]
+		public void FieldOnMissingTypeIsReported()
+		{
+			// arrange
+			Assembly a = AssemblyGenerator.Load("public class A { public int i; }").Assemblies.First();
+			Assembly b = AssemblyGenerator.Load("class A { public int i; }").Assemblies.First();
+
+			// act
+			var issues = raiser.GetCompatibilityIssuesBetween(a, b).ToList();
+
+			Assert.AreEqual(0, issues.Count);
+		}
+	}
+}

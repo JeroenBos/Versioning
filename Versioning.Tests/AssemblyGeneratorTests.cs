a6a9d4987b20854cd46d929edd3bca6e24f9a8fc
JeroenBos <jer123bos@gmail.com>
2019-11-04 20:47:21 +0100

    Rename AssemblyGeneratorTests

--- Versioning.Tests/AssemblyGeneratorTests.cs
+++ Versioning.Tests/AssemblyGeneratorTests.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 
 namespace Versioning.Tests
 {
-	public class Tests
+	public class AssemblyGeneratorTests
 	{
 		[Test]
 		public void CanCreateEmptyAssembly()
@@ -16,17 +16,6 @@ namespace Versioning.Tests
 		}
 		[Test]
 		public void CanUseTypeFromSeparatelyLoadedAssembly()
-		{
-			/// This also checks that internally the overload <see cref="Load(AssemblyName)"/> isn't used when loading from stream
-			var assemblyContext = AssemblyGenerator.Load("public class A { }", "assemblyname");
-			var assembly = assemblyContext.Assemblies.First();
-
-			Assert.AreEqual("A", assembly.GetTypes()[0].Name);
-		}
-
-
-		[Test]
-		public void TestMissingTypeIssueRaiser()
 		{
 			/// This also checks that internally the overload <see cref="Load(AssemblyName)"/> isn't used when loading from stream
 			var assemblyContext = AssemblyGenerator.Load("public class A { }", "assemblyname");

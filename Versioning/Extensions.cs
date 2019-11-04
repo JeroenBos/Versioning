18066f35b6a926196d3a6cd621ea3386d9d3b082
JeroenBos <jer123bos@gmail.com>
2019-11-04 20:46:26 +0100

    Implement some PropertyInfo extension methods

--- Versioning/Extensions.cs
+++ Versioning/Extensions.cs
@@ -28,15 +28,21 @@ namespace Versioning
 		}
 
 		/// <summary>
-		/// Gets the bindingflags for public and static, if present on the specified info.
+		/// Gets the bindingflags for public/nonpublic and static/instance of the specified member info.
 		/// </summary>
-		public static BindingFlags GetAccessModifiers(this MethodBase info)
+		public static BindingFlags GetAccessModifiers(this MemberInfo info)
 		{
 			if (info == null) throw new ArgumentNullException(nameof(info));
 
-			var result = info.IsStatic ? BindingFlags.Static : BindingFlags.Instance;
-			result |= info.IsPublic ? BindingFlags.Public : BindingFlags.NonPublic;
-			return result;
+			var (isStatic, isPublic) = info switch
+			{
+				MethodBase method => (method.IsStatic, method.IsPublic),
+				PropertyInfo property => (property.IsStatic(), property.IsFamily()),
+				_ => throw new NotImplementedException()
+			};
+
+			return (isStatic ? BindingFlags.Static : BindingFlags.Instance)
+				 | (isPublic ? BindingFlags.Public : BindingFlags.NonPublic);
 		}
 
 		/// <summary>
@@ -60,6 +66,49 @@ namespace Versioning
 				return all.Where(m => m.IsFamily);
 			else
 				return all;
+		}
+
+		/// <summary>
+		/// Gets all constructors on the specified type with the same public, protected and static modifiers as the specified info.
+		/// </summary>
+		public static IEnumerable<PropertyInfo> GetPropertiesAccessibleLike(this Type type, PropertyInfo info)
+		{
+			var all = type.GetProperties(info.GetAccessModifiers());
+			if (info.IsFamily())
+				return all.Where(IsFamily);
+			else
+				return all;
+		}
+
+		/// <summary>
+		/// Gets whether the property has the access modifier 'protected'.
+		/// </summary>
+		public static bool IsFamily(this PropertyInfo propertyInfo)
+		{
+			if (propertyInfo == null) throw new ArgumentNullException(nameof(propertyInfo));
+
+			return !propertyInfo.IsPublic()
+				&& propertyInfo.GetAccessors(true).Any(accessor => accessor.IsFamily);
+		}
+
+		/// <summary>
+		/// Gets whether the property has the access modifier 'static'.
+		/// </summary>
+		public static bool IsStatic(this PropertyInfo propertyInfo)
+		{
+			if (propertyInfo == null) throw new ArgumentNullException(nameof(propertyInfo));
+
+			return propertyInfo.GetAccessors(true).Any(accessor => accessor.IsStatic);
+		}
+
+		/// <summary>
+		/// Gets whether the property has the access modifier 'public'.
+		/// </summary>
+		public static bool IsPublic(this PropertyInfo propertyInfo)
+		{
+			if (propertyInfo == null) throw new ArgumentNullException(nameof(propertyInfo));
+
+			return propertyInfo.GetAccessors(true).Any(accessor => accessor.IsPublic);
 		}
 	}
 }

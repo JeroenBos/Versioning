9e185e1e2fdbfb3fd7dba0693bcc522fe03a7fce
JeroenBos <jer123bos@gmail.com>
2019-11-06 09:34:56 +0100

    List field references

--- UsageDetector.Tests/Exploratory.cs
+++ UsageDetector.Tests/Exploratory.cs
@@ -13,7 +13,10 @@ namespace Versioning.UsageDetector.Tests
 		{
 			const string source = @"class C { int i = System.Math.Abs(0); }";
 			var assembly = AssemblyDefinition.ReadAssembly(AssemblyGenerator.CreateAssembly(source));
-			var allMethodCalls = ListAllReferences.GetAllMethodReferences(assembly).OrderBy(r => r.Name).ToList();
+			var allMethodCalls = ListAllReferences.GetAllMemberReferences(assembly)
+				                                  .OfType<MethodReference>()
+				                                  .OrderBy(r => r.Name)
+				                                  .ToList();
 
 			Assert.AreEqual(2, allMethodCalls.Count);
 			Assert.AreEqual(".ctor", allMethodCalls[0].Name);
@@ -34,11 +37,35 @@ class C {
 	}
 }";
 			var assembly = AssemblyDefinition.ReadAssembly(AssemblyGenerator.CreateAssembly(source));
-			var allMethodCalls = ListAllReferences.GetAllMethodReferences(assembly).OrderBy(r => r.Name).ToList();
+			var allMethodCalls = ListAllReferences.GetAllMemberReferences(assembly)
+				                                  .OfType<MethodReference>()
+				                                  .OrderBy(r => r.Name)
+				                                  .ToList();
 
 			Assert.AreEqual(2, allMethodCalls.Count);
 			Assert.AreEqual(".ctor", allMethodCalls[0].Name);
 			Assert.AreEqual("Abs", allMethodCalls[1].Name);
+		}
+
+		[Test]
+		public void ListFieldReference()
+		{
+			const string source = @"
+class C {
+	static int i;
+    void m()
+    {
+        int j = i;
+    }
+}";
+			var assembly = AssemblyDefinition.ReadAssembly(AssemblyGenerator.CreateAssembly(source));
+			var refs = ListAllReferences.GetAllMemberReferences(assembly)
+										.OfType<FieldReference>()
+				                        .OrderBy(r => r.Name)
+				                        .ToList();
+
+			Assert.AreEqual(1, refs.Count);
+			Assert.AreEqual("i", refs[0].Name);
 		}
 	}
 }
--- UsageDetector/ListAllReferences.cs
+++ UsageDetector/ListAllReferences.cs
@@ -8,7 +8,7 @@ namespace Versioning.UsageDetector
 {
 	public static class ListAllReferences
 	{
-		public static IEnumerable<MethodReference> GetAllMethodReferences(AssemblyDefinition assembly)
+		public static IEnumerable<MemberReference> GetAllMemberReferences(AssemblyDefinition assembly)
 		{
 			// it suffices to only check in method bodies, because property/event accessors are implemented as methods as well,
 			// and e.g. field initializers are in the constructor. Local functions are also lowered to nonlocal functions, so it's fine
@@ -16,10 +16,8 @@ namespace Versioning.UsageDetector
 				   from MethodDefinition method in type.Methods
 				   where method.HasBody
 				   from Instruction instruction in method.Body.Instructions
-				   where instruction.OpCode == OpCodes.Call
-				   let methodReference = instruction.Operand as MethodReference
-				   where methodReference != null
-				   select methodReference;
+				   where instruction.Operand is MemberReference reference
+				   select instruction.Operand as MemberReference; ; // Hm. The name 'reference' does not exist in the current context
 		}
 	}
 }

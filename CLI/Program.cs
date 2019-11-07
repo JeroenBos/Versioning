72589320fbff293fce7fd74b0f4e8e5b78f391d1
JeroenBos <jer123bos@gmail.com>
2019-11-07 16:34:56 +0100

    Create initial CLI verion

---
+++ CLI/Program.cs
@@ -0,0 +1,56 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using IssueDetector = Versioning.UsageDetector.UsageDetector;
+
+namespace Versioning.CLI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length != 3)
+			{
+				Console.WriteLine("Expected 3 argument:, paths to main assembly, dependent assembly and dependent assembly of higher version");
+				Console.ReadKey();
+				return;
+			}
+
+			var assemblies = args.Select(arg =>
+			{
+				try
+				{
+					return AssemblyDefinition.ReadAssembly(arg);
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.Message);
+					return null;
+				}
+			}).ToList();
+
+			if (assemblies.Any(a => a == null))
+			{
+				Console.ReadKey();
+				return;
+			}
+
+			var (main, dependency, dependencyHigherVersion) = (assemblies[0], assemblies[1], assemblies[2]);
+
+
+			var issueCollector = CompatiblityIssueCollector.MissingMembersIssueCollector;
+			var detectedIssues = IssueDetector.DetectCompatibilityIssues(issueCollector, main, dependency, dependencyHigherVersion)
+				                              .ToList();
+
+			Console.WriteLine($"Detected {detectedIssues.Count} issues:");
+			foreach(var detectedIssue in detectedIssues)
+			{
+				Console.WriteLine(detectedIssue.ToDisplayString());
+			}
+			Console.WriteLine("Done");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ CLI/WriteToConsole.cs
@@ -0,0 +1,27 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using Versioning.UsageDetector;
+
+namespace Versioning.CLI
+{
+	public static class WriteToConsoleExtensions
+	{
+		public static string ToDisplayString(this IDetectedCompatibilityIssue issue)
+		{
+			if(issue == null) throw new ArgumentNullException(nameof(issue));
+
+			// this method merely dispatches
+			switch (issue.Issue)
+			{
+				case null: throw new ArgumentException();
+				case IMissingMemberCompatibilityIssue m: return m.ToDisplayString(issue.Locations); 
+				default: return $"An unhandled issue of type '${issue.GetType()}' was detected";
+			};
+		}
+		public static string ToDisplayString(this IMissingMemberCompatibilityIssue issue, IReadOnlyList<MemberReference> locations)
+		{
+			return $"'{issue.MissingMember}' was not present in the newer dependency, and is referenced {locations.Count} times in the dependent assembly.";
+		}
+	}
+}

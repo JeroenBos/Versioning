eae9df806bce8bb5082c81967c0e8b9a3729cde4
JeroenBos <jer123bos@gmail.com>
2019-11-13 13:37:58 +0100

    Facilitate testing both issues and runtime exception

--- UsageDetector.Tests/Demonstrations.cs
+++ UsageDetector.Tests/Demonstrations.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Mono.Cecil;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,16 +12,39 @@ namespace Versioning.UsageDetector.Tests
 		/// <summary>
 		/// Loads the main assembly built against the dependency v1, but runtime loads dependency v2, and returns the main assembly's entry point as action, if any.
 		/// </summary>
-		public static TestDelegate? LoadAssemblyWithReferenceAgainstDifferentVersion(string sourceCode_DependencyV1, string sourceCode_DependencyV2, string sourceCode_Main)
+		public static TestDelegate? LoadAssemblyWithReferenceAgainstDifferentVersion(
+			string sourceCode_DependencyV1,
+			string sourceCode_DependencyV2,
+			string sourceCode_Main,
+			out (AssemblyDefinition dependencyV1, AssemblyDefinition dependencyV2, AssemblyDefinition main) assemblyDefinitions)
 		{
-			var assemblies = AssemblyGenerator.LoadAssemblyWithReferenceAgainstDifferentVersion(sourceCode_DependencyV1, sourceCode_DependencyV2, sourceCode_Main).Assemblies.ToList();
+			var assemblies = AssemblyGenerator.LoadAssemblyWithReferenceAgainstDifferentVersion(sourceCode_DependencyV1, sourceCode_DependencyV2, sourceCode_Main, out assemblyDefinitions).Assemblies.ToList();
 
 			var mainAssembly = assemblies[1];
 			if (mainAssembly.EntryPoint == null)
 				return null;
 			return () => mainAssembly.EntryPoint.Invoke(null, new object?[] { null });
 		}
-		
+
+		/// <summary>
+		/// Loads the main assembly built against the dependency v1, but runtime loads dependency v2, and returns the main assembly's entry point as action, if any.
+		/// Also returns raised issues.
+		/// </summary>
+		/// <param name="issueRaiser"> Specify null to use the default issue collector. </param>
+		public static (TestDelegate? entryPoint, IReadOnlyList<IDetectedCompatibilityIssue> issues) DetectIssuesAndLoadAssemblyWithReferenceAgainstDifferentVersion(
+			string sourceCode_DependencyV1,
+			string sourceCode_DependencyV2,
+			string sourceCode_Main,
+			CompatiblityIssueCollector? issueRaiser = null)
+		{
+			TestDelegate? entryPoint = LoadAssemblyWithReferenceAgainstDifferentVersion(sourceCode_DependencyV1, sourceCode_DependencyV2, sourceCode_Main, out var assemblyDefinitions);
+			var (dependencyV1, dependencyV2, main) = assemblyDefinitions;
+
+			var issues = UsageDetector.DetectCompatibilityIssues(issueRaiser ?? CompatiblityIssueCollector.Default, main, dependencyV1, dependencyV2).ToList();
+
+			return (entryPoint, issues);
+		}
+
 		[Test]
 		public void RunMain()
 		{
@@ -28,9 +52,10 @@ namespace Versioning.UsageDetector.Tests
 			const string sourceCode_DependencyV2 = @"";
 			const string sourceCode_Main = @"public class Program { public static void Main(string[] args) { } }";
 
-			TestDelegate entryPoint = LoadAssemblyWithReferenceAgainstDifferentVersion(sourceCode_DependencyV1, sourceCode_DependencyV2, sourceCode_Main)!;
+			var (entryPoint, detectedIssues) = DetectIssuesAndLoadAssemblyWithReferenceAgainstDifferentVersion(sourceCode_DependencyV1, sourceCode_DependencyV2, sourceCode_Main);
 
-			entryPoint();
+			Assert.AreEqual(0, detectedIssues.Count);
+			Assert.DoesNotThrow(entryPoint);
 		}
 	}
 }
